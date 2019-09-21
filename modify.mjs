3134a7efa2e4e8aa1023559130125d06fb57cc82
Kris Gano <46331884+pyxld-kris@users.noreply.github.com>
2019-09-21 13:46:08 -0500

    Added numbered blocks

--- modify.mjs
+++ modify.mjs
@@ -15,7 +15,16 @@
  */
 /**************** Start Modifying Here! *********************/
 
-var stageArray = [1,1,1,1,1,1,1,1,1,1,1,1];
+var stageArray = [5,3,6,2,7,6,8,9,4,11,10,1];
+
+// Now sort the array!
+function doArraySort(stageArray) {
+  // Write sorting logic here
+
+  return stageArray;
+}
+
+doArraySort(stageArray);
 
 
 /**** GOODBYE! ************************************************/
@@ -41,13 +50,15 @@ let gameHeight = scene.game.config.height; // easy access and readability
 let blockSize = parseInt(gameWidth/stageArray.length);
 scene.solidBlocks = [];
 for (let i=0; i<stageArray.length; i++) {
-  if (stageArray[i] === 1) {
+  if (stageArray[i] != 0) {
     // Create a block
     let newBlock = scene.physics.add.staticSprite(
         blockSize/2+blockSize*i+2,
         100,
         this.generateRectangleSprite(blockSize-4, blockSize-4)
       )
+    let blockLabel = scene.add.text(blockSize/2+blockSize*i+2, 100, stageArray[i], {fill:"#000000"})
+      .setOrigin(.5);
     scene.solidBlocks.push(newBlock);
   }
 }
